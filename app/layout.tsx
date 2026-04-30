81b6dee0d9eac101a2a8503c76a23da104c1cb6b
saranj <saranj.bule@gmail.com>
2026-04-30 15:52:42 +0530

    Microsoft clarity and GA script added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { geistMono, geistSans } from './fonts';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
     title: 'Saranju Bule',
@@ -61,6 +62,28 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                {/* Load Google Analytics script */}
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-62W25D8BR5"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-62W25D8BR5');`}
+                </Script>
+
+                {/* Load Microsoft Clarity script */}
+                <Script id="microsoft-clarity" strategy="afterInteractive">
+                    {`(function(c,l,a,r,i,t,y){
+                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                        })(window, document, "clarity", "script", "wjqiky4bok");`}
+                </Script>
+            </head>
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
